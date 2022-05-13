3f16b840db4a5536892e3815e88c821bb87ef744
john-shupe-amteck <jshupe@amteck.com>
2022-05-13 12:07:51 -0400

    format adjustment

--- src/main.cpp
+++ src/main.cpp
@@ -26,10 +26,10 @@ int main()
 	activeWindow = &header_win;
 	Div footer_win("Key Bindings", LINES - 3, 0, 3, COLS);
 
-	Text key_bind_1(&footer_win, "F1: Quit", 1, 2);
-	Text key_bind_2(&footer_win, "N: New Rental", 1, key_bind_1.lastCol() + 2);
-	Text key_bind_3(&footer_win, "F: Filter", 1, key_bind_2.lastCol() + 2);
-	Text key_bind_4(&footer_win, "E: Extend", 1, key_bind_3.lastCol() + 2);
+	Text key_bind_1(&footer_win, "F1:Quit", 1, 2);
+	Text key_bind_2(&footer_win, "N:New Rental", 1, key_bind_1.lastCol() + 2);
+	Text key_bind_3(&footer_win, "F:Filter", 1, key_bind_2.lastCol() + 2);
+	Text key_bind_4(&footer_win, "E:Extend", 1, key_bind_3.lastCol() + 2);
 
 	string headers = "PO Number,Description,Price,Start Date,End Date ";
 	string widths = "14,60,10,11,11";
